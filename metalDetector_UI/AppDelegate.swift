aa2252d70cef9da41943c9d0feb12c66500cc82c
kevinvm093 <kvallejo093@me.com>
2018-05-25 22:16:31 -0400

    Initial Commit

---
+++ metalDetector_UI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  metalDetector_UI
+//
+//  Created by Kevin Vallejo on 5/25/18.
+//  Copyright © 2018 Vallejo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ metalDetector_UI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  metalDetector_UI
+//
+//  Created by Kevin Vallejo on 5/25/18.
+//  Copyright © 2018 Vallejo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
